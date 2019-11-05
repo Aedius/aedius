4fa69e894109f26798c21dd03dfd74aa15b31fb4
Aedius <aedius.filmania@gmail.com>
2019-11-05 22:27:06 +0100

    change helm version

--- .helm/deploy.sh
+++ .helm/deploy.sh
@@ -4,24 +4,24 @@ set -e
 
 RELEASE_NAME="ms-cart-${TRAVIS_BRANCH}"
 
-#DEPLOY_FAILED=$(helm list --failed "^${RELEASE_NAME}\$")
-#
-#echo "trying to deploy ${RELEASE_NAME} ..."
-#
-#if [ -n "${DEPLOY_FAILED}" ]
-#then
-#	echo "previous deployment failed, deleting it..."
-#	helm delete --purge "${RELEASE_NAME}"
-#
-#    DEPLOY_EXISTS=$(helm list --all "^${RELEASE_NAME}\$")
-#
-#    until [ -z "${DEPLOY_EXISTS}" ]
-#    do
-#        echo "waiting delete deployment to finish..."
-#        sleep 30
-#        DEPLOY_EXISTS=$(helm list --all "^${RELEASE_NAME}\$")
-#    done
-#fi
+DEPLOY_FAILED=$(helm list --failed "^${RELEASE_NAME}\$")
+
+echo "trying to deploy ${RELEASE_NAME} ..."
+
+if [ -n "${DEPLOY_FAILED}" ]
+then
+	echo "previous deployment failed, deleting it..."
+	helm delete --purge "${RELEASE_NAME}"
+
+    DEPLOY_EXISTS=$(helm list --all "^${RELEASE_NAME}\$")
+
+    until [ -z "${DEPLOY_EXISTS}" ]
+    do
+        echo "waiting delete deployment to finish..."
+        sleep 30
+        DEPLOY_EXISTS=$(helm list --all "^${RELEASE_NAME}\$")
+    done
+fi
 
 
 echo "deployment already exists, upgrading it..."
