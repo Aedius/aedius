b86d37366ae34a5acf647c0070ba833fd92c2df8
Aedius <aedius.filmania@gmail.com>
2019-11-05 22:08:48 +0100

    helm cleanup

--- .helm/deploy.sh
+++ .helm/deploy.sh
@@ -4,8 +4,7 @@ set -e
 
 RELEASE_NAME="ms-cart-${TRAVIS_BRANCH}"
 
-DEPLOY_EXISTS=$(helm ls --all "^${RELEASE_NAME}\$")
-DEPLOY_FAILED=$(helm ls --failed "^${RELEASE_NAME}\$")
+DEPLOY_FAILED=$(helm list --failed "^${RELEASE_NAME}\$")
 
 echo "trying to deploy ${RELEASE_NAME} ..."
 
@@ -14,23 +13,18 @@ then
 	echo "previous deployment failed, deleting it..."
 	helm delete --purge "${RELEASE_NAME}"
 
-    DEPLOY_EXISTS=$(helm ls --all "^${RELEASE_NAME}\$")
+    DEPLOY_EXISTS=$(helm list --all "^${RELEASE_NAME}\$")
 
     until [ -z "${DEPLOY_EXISTS}" ]
     do
         echo "waiting delete deployment to finish..."
         sleep 30
-        DEPLOY_EXISTS=$(helm ls --all "^${RELEASE_NAME}\$")
+        DEPLOY_EXISTS=$(helm list --all "^${RELEASE_NAME}\$")
     done
 fi
 
-if [ -n "${DEPLOY_EXISTS}" ]
-then
-	echo "deployment already exists, upgrading it..."
-	helm upgrade "${RELEASE_NAME}" --namespace "static" .helm/aedius/ \
-		--set images.aedius.tag=${TRAVIS_BUILD_NUMBER}
-else
-	echo "deployment doesn't exist yet, installing it..."
-	helm install --name "${RELEASE_NAME}" --namespace "static" .helm/aedius/ \
-		--set images.aedius.tag=${TRAVIS_BUILD_NUMBER}
-fi
+
+echo "deployment already exists, upgrading it..."
+helm upgrade --install "${RELEASE_NAME}" --namespace "static" .helm/aedius/ \
+  --set images.aedius.tag=${TRAVIS_BUILD_NUMBER}
+
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-build on [Travis](https://travis-ci.org/Aedius/aedius) ![https://travis-ci.org/Aedius/aedius](https://travis-ci.org/Aedius/aedius.svg?branch=master)
+[![https://travis-ci.org/Aedius/aedius](https://travis-ci.org/Aedius/aedius.svg?branch=master)](https://travis-ci.org/Aedius/aedius) 
 
 # aedius
 personnal website
